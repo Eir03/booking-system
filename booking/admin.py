58231074c37bd88b616ceedb1c9050abb119b2c8
Староверов Данила Андреевич <staroverovdanila@gmail.com>
2024-03-23 20:25:45 +0300

    Fix register models to admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django import apps
+from django.db import models
 
-# Register your models here.
+def register_model(model: models.Model) -> models.Model:
+    """Регистрирует модель данных в административную панель
+
+    Args:
+        model (django.db.models.Model): Класс модели данных в Django
+
+    Returns:
+        django.db.models.Model: Модель данных
+    """
+    admin.site.register(model)
+    return model
--- booking/models.py
+++ booking/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
-
+from .admin import register_model
+@register_model
 class Service(models.Model):
     name = models.CharField('Наименование услуги', blank=True, max_length=100)
     price = MoneyField('Стоимость', max_digits=10, decimal_places=2, default_currency='RUB')
 
+    def __str__(self):
+        return self.name
+
+@register_model
 class RoomType(models.Model):
     name = models.CharField('Наименование', max_length=100)
     image = models.ImageField('Изображение', blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
+@register_model
 class Room(models.Model):
     number = models.CharField('Номер', max_length=5)
     type = models.ForeignKey('RoomType', verbose_name='Тип номера', on_delete=models.CASCADE)
     max_occupancy = models.PositiveSmallIntegerField('Количество мест')
     price = MoneyField('Стоимость', max_digits=10, decimal_places=2, default_currency='RUB')
 
+    def __str__(self):
+        return self.number
+
+@register_model
 class Reservation(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     check_in = models.DateField('Дата въезда')
@@ -22,3 +35,6 @@ class Reservation(models.Model):
     occupancy = models.PositiveSmallIntegerField('Вместимость')
     total_price = MoneyField('Стоимость', max_digits=10, decimal_places=2, default_currency='RUB')
     services = models.ManyToManyField('Service')
+
+    def __str__(self):
+        return self.name
